a821f0fa68e10ce14e024d376ba1d6f3aafd0533
NDUBA <user@users-MacBook-Pro.local>
2024-03-05 09:29:35 +0300

    done

--- lib/testing/lib_test.py
+++ lib/testing/lib_test.py
@@ -99,6 +99,7 @@ class TestCalculator:
         '''prints "Invalid operation!" and returns None if operation invalid'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        assert(calculator('a', 1, 2) == None)
+        calculator('a', 1, 2)
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Invalid operation!\n")
+        assert captured_out.getvalue().strip() == "Invalid operation!"
+
